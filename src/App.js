7a0c16df6f8e0896215884d7f32f9dff955a8f69
Aitor Gomez Goiri <aitor@gomezgoiri.net>
2017-10-15 12:18:54 +0200

    Changing location frequently
    This forces error "Cannot read property 'overlayMouseTarget' of undefined" (see https://github.com/tomchentw/react-google-maps/issues/482).

--- src/App.js
+++ src/App.js
@@ -3,7 +3,36 @@ import React, { Component } from 'react'
 import { LocationViewer } from './Location'
 import './App.css'
 
+const getLocation = () => ({
+  lng: -2.935013 + (Math.random() * 0.03),
+  lat: 43.262985 + (Math.random() * 0.03)
+})
+
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      updater: null,
+      position: { lng: -2.935013, lat: 43.262985 }
+    }
+  }
+
+  componentDidMount() {
+    const updater = setInterval(() => {
+      this.setState({
+        position: getLocation()
+      })
+    }, 100)
+    this.setState({ updater })
+  }
+
+  componentWillUnmount() {
+    if (this.state.updater !== null) {
+      clearInterval(this.state.updater)
+    }
+  }
+
   render() {
     return (
       <div className='App'>
@@ -11,9 +40,7 @@ class App extends Component {
           Sample app used to isolate the react-google-maps error.
         </p>
         <LocationViewer
-          marker={{
-            position: { lng: -2.935013, lat: 43.262985 }
-          }}
+          marker={{ position: this.state.position }}
         />
       </div>
     )
